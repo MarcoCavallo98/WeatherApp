b312eaf24c8e4d93a487da3b5557b43b610cde30
Marco Cavallo <cavallo.marco0@gmail.com>
2021-07-15 14:22:18 +0200

    Aggiungere i file di progetto.

---
+++ Model/DBManager.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
+using System.Text.Json;
+using System.Diagnostics;
+
+namespace WeatherApp.Model
+{
+    class DBManager
+    {
+
+        private string PATH = "./Resource/WeatherMapConfig.json";
+
+        private string DBPath;
+
+        private SQLiteConnection conn;
+
+        private static DBManager manager;
+
+        private DBManager() { }
+
+
+        public async void CreateDB() 
+        {
+            if (DBPath == null)
+                await ApplyConfig();
+
+            if (!File.Exists(DBPath))
+            {
+                try
+                {
+                    SQLiteConnection.CreateFile(DBPath);
+                    DBConnect();
+                    SQLiteCommand command = conn.CreateCommand();
+                    command.CommandText = @"
+                                            CREATE TABLE Cities(
+                                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                                name VARCHAR(50)
+                                            );
+                                       ";
+                    await command.ExecuteNonQueryAsync();
+                }
+                catch (Exception e) 
+                {
+                    throw;
+                }
+            }
+        }
+
+        public async void DBConnect() 
+        {
+            if (DBPath == null)
+                await ApplyConfig();
+
+            if (conn == null)
+            {
+                try
+                {
+                    conn = new SQLiteConnection($"Data Source={DBPath};");
+                    conn.Open();
+                }
+                catch (Exception e) 
+                {
+                    throw;
+                }
+            }
+        }
+
+        private async Task ApplyConfig() 
+        {
+            FileStream fs = File.OpenRead(PATH);
+            JsonElement data = await JsonSerializer.DeserializeAsync<JsonElement>(fs);
+            string name = data.GetProperty("DBName").ToString().Trim();
+            string path = data.GetProperty("DBPath").ToString().Trim();
+            DBPath = $"./{path}/{name}.db";
+        }
+
+        public static DBManager getIstance() 
+        {
+            if (manager == null)
+                manager = new DBManager();
+
+            return manager;
+        }
+
+    }
+}
---
+++ Model/WeatherMapManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Net.Http;
+
+namespace WeatherApp.Model
+{
+    sealed class WeatherMapManager
+    {
+        private string PATH = "./Resource/WeatherMapConfig.json";
+
+        private string APIKey;
+
+        private static WeatherMapManager manager;
+
+        private HttpClient cl = new HttpClient();
+
+        private WeatherMapManager()
+        {
+        }
+
+        private async Task ApplyConfig() 
+        {
+            if (APIKey == null)
+            {
+                FileStream fs = File.OpenRead(PATH);
+                JsonElement data = await JsonSerializer.DeserializeAsync<JsonElement>(fs);
+                APIKey = data.GetProperty("APIKey").ToString().Trim();
+            }
+        }
+
+        public static WeatherMapManager getInstance() 
+        {
+            if (manager == null)
+                manager = new WeatherMapManager();
+
+            return manager;
+        }
+
+        public async Task<string> GetCityWeather(string CityName, bool FiveDays) 
+        {
+            await ApplyConfig();
+            Uri url;
+            url = FiveDays == true ? Uris.FiveDays(CityName, APIKey) : Uris.CurrenTime(CityName, APIKey);
+            HttpResponseMessage res = await cl.GetAsync(url);
+            string body = await res.Content.ReadAsStringAsync();
+            return body;
+        }
+
+        private static class Uris 
+        { 
+            public static Uri CurrenTime(string CityName, string APIKey) 
+            {
+                Uri url = new Uri($"http://api.openweathermap.org/data/2.5/weather?q={CityName}&appid={APIKey}");
+                return url;
+            }
+
+            public static Uri FiveDays(string CityName, string APIKey) 
+            {
+                Uri url = new Uri($"http://api.openweathermap.org/data/2.5/forecast?q={CityName}&appid={APIKey}");
+                return url;
+            }
+        }
+    }
+}
